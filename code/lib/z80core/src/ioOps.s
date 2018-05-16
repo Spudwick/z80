61d2cd315bbee3d6f7cd2f66013c667d64d479fc
Spudwick <32262684+Spudwick@users.noreply.github.com>
2018-05-16 14:36:51 +0100

    Misc
    Miscelaneous changes

--- code/lib/z80core/src/ioOps.s
+++ code/lib/z80core/src/ioOps.s
@@ -1,14 +1,14 @@
 __write_port::
 ; IN : unsigned char port	port -> (sp - 1)
 ; IN : unsigned char data	data -> (sp - 2)
-	push	ix			; ixh -> (sp - 1) ixl -> (sp - 2)
-	ld	ix,#0			; 0 -> ix
-	add	ix,sp			; ix + sp -> ix
+	push	ix								; ixh -> (sp - 1) ixl -> (sp - 2)
+	ld	ix,#0								; 0 -> ix
+	add	ix,sp								; ix + sp -> ix
 
-	ld	c,4 (ix)		; (ix + 4) -> c		<port -> c>
-	ld	b,5 (ix)		; (ix + 5) -> b		<data -> b>
+	ld	c,4 (ix)							; (ix + 4) -> c		<port -> c>
+	ld	b,5 (ix)							; (ix + 5) -> b		<data -> b>
 
-	out	(c),b			; b -> (c)		<data -> (port)>
+	out	(c),b								; b -> (c)		<data -> (port)>
 
 	pop	ix
 	ret
@@ -16,14 +16,14 @@ __write_port::
 
 __read_port::
 ; IN : unsigned char port	port -> (sp - 1)
-	push	ix			; ixh -> (sp - 1) ixl -> (sp - 2)
-	ld	ix,#0			; 0 -> ix
-	add	ix,sp			; ix + sp -> ix
+	push	ix								; ixh -> (sp - 1) ixl -> (sp - 2)
+	ld	ix,#0								; 0 -> ix
+	add	ix,sp								; ix + sp -> ix
 
-	ld	c,4 (ix)		; (ix + 4) -> c		<port -> c>
+	ld	c,4 (ix)							; (ix + 4) -> c		<port -> c>
 
-	in	b,(c)			; (c) -> a		<(port) -> b>
-	ld	l,b			; b -> l
+	in	b,(c)								; (c) -> a		<(port) -> b>
+	ld	l,b									; b -> l
 
 	pop	ix
 	ret
