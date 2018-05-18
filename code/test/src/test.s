dedf6f0afe564206fb8751124c02f54a2bfbec48
Spudwick <32262684+Spudwick@users.noreply.github.com>
2018-05-18 16:11:59 +0100

    Windows build support
    Finished adding support for building on windows.
    Still need to check that Linux building hasn't broken.

---
+++ code/test/src/test.s
@@ -0,0 +1,5 @@
+.area		_BOOT (ABS)
+.org		0x8000
+
+_entry_place ::
+	ld	a,#0x20
---
+++ code/test/test.mk
@@ -0,0 +1,6 @@
+SELFDIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+
+AREA := 
+MAINFILE :=
+LIBRARIES := 
+OUTPUT := lnk/test.ihx
