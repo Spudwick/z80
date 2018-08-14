de01de8c4e4cbdb9167a7f1e74cce4d692bbd8b3
Spudwick <32262684+Spudwick@users.noreply.github.com>
2018-08-14 16:14:52 +0100

    Small changes

--- code/build-sys/build-dirs.mk
+++ code/build-sys/build-dirs.mk
@@ -4,6 +4,8 @@
 ifeq ($(OS),Windows_NT)
 GET_CWD			 = $(subst \,/,$(shell cd)/)
 OS_SYN       	 = $(subst /,\,$(1))
+MK_SYN       	 = $(subst \,/,$(1))
+GET_SDCC_DIR	 = $(subst sdcc.exe,,$(subst \,/,$(shell where sdcc)))
 
 # LINUX
 else
@@ -15,12 +17,15 @@ endif
 # DIRECTORY DEFINES: ====================================================================
 
 SELF_DIR		:= $(call GET_CWD)
-BUILDSYS_DIR 	:= $(SELF_DIR)/build-sys/
+BUILDSYS_DIR 	:= $(SELF_DIR)build-sys/
 
+SRC_TREE		:= src/
+INC_TREE		:= inc/
 OUT_TREE		:= out/
-PP_TREE			:= $(OUT_TREE)/pp/
-ASM_TREE		:= $(OUT_TREE)/asm/
-REL_TREE		:= $(OUT_TREE)/rel/
-LNK_TREE		:= $(OUT_TREE)/lnk/
+PP_TREE			:= $(OUT_TREE)pp/
+DEP_TREE		:= $(PP_TREE)dep/
+ASM_TREE		:= $(OUT_TREE)asm/
+REL_TREE		:= $(OUT_TREE)rel/
+LNK_TREE		:= $(OUT_TREE)lnk/
 
 # =======================================================================================
--- code/build-sys/build-tools.mk
+++ code/build-sys/build-tools.mk
@@ -1,8 +1,15 @@
+include build-sys/build-dirs.mk
+
 # TOOLS AND DEFAULT TOOL FLAGS :=========================================================
 
 # C Pre-Processor and Flags:
-CPP			:= sdcpp
-CPPFLAGS	:= -M
+CPP				:= sdcpp
+CPPFLG_STD		:= -nostdinc -Wall -std=c99
+CPPFLG_LCLINC	:= $(addprefix -I$(SELF_DIR),$(addsuffix $(INC_TREE),$(wildcard lib/*/)))
+CPPFLG_SYSINC	:= -isystem "$(call OS_SYN,$(call GET_SDCC_DIR)include/z80)" -isystem "$(call OS_SYN,$(call GET_SDCC_DIR)include)"
+CPPFLG_DEF		:= -obj-ext=.rel -D__SDCC_STACK_AUTO -D__SDCC_CHAR_UNSIGNED -D__SDCC_INT_LONG_REENT -D__SDCC_FLOAT_REENT -D__SDCC=3_6_0 -D__SDCC_VERSION_MAJOR=3 -D__SDCC_VERSION_MINOR=6 -D__SDCC_VERSION_PATCH=0 -DSDCC=360 -D__SDCC_REVISION=9615 -D__SDCC_z80 -D__STDC_NO_COMPLEX__=1 -D__STDC_NO_THREADS__=1 -D__STDC_NO_ATOMICS__=1 -D__STDC_NO_VLA__=1 -D__STDC_ISO_10646__=201409L -D__STDC_UTF_16__=1 -D__STDC_UTF_32__=1
+CPPFLG_DEP		:= -M -MP
+CPPFLAGS		:= $(CPPFLG_STD) $(CPPFLG_LCLINC) $(CPPFLG_SYSINC) $(CPPFLG_DEF) $(CPPFLG_DEP)
 
 # C Compiler and Flags:
 CC      := sdcc
