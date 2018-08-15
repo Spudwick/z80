6743d86b0cef2682b503be85d77c75ffda156475
Spudwick <32262684+Spudwick@users.noreply.github.com>
2018-08-15 12:29:31 +0100

    Basic functionality

--- code/build-sys/build-sources.mk
+++ code/build-sys/build-sources.mk
@@ -1,7 +1,25 @@
 # SOURCE FILE TOOLS: ======================================================
 
-define ADD_SOURCE =
-@echo test1;\
-@echo test2
+define TEST1
+TST1 += $(1:%.c=%.o)
+TST2 += $(1:%.c=%.cpp)
+$1 : test.test
 endef
-export ADD_SOURCE
+
+TEST2 =									\
+$(eval TST1 += $(1:%.c=%.o2))			\
+$(eval TST2 += $(1:%.c=%.cpp2))
+
+define ADD_CSRC
+SRCS_C += $(1)
+ASMS += $(ASM_DIR)$(notdir $(1:%.c=%.s))
+OBJS += $(OBJ_DIR)$(notdir $(1:%.c=%.rel))
+$(ASM_DIR)$(notdir $(1:%.c=%.s)) : $(1)
+$(OBJ_DIR)$(notdir $(1:%.c=%.rel)) : $(ASM_DIR)$(notdir $(1:%.c=%.s))
+endef
+
+define ADD_SSRC
+SRCS_S += $(1)
+OBJS += $(OBJ_DIR)$(notdir $(1:%.s=%.rel))
+$(OBJ_DIR)$(notdir $(1:%.s=%.rel)) : $(1)
+endef
