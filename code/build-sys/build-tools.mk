d57c1b4afd7390b527271f4927a1fb14f3b656ee
Spudwick <32262684+Spudwick@users.noreply.github.com>
2019-03-15 16:30:04 +0000

    Windows updates
    Probably broken linux now XD

--- code/build-sys/build-tools.mk
+++ code/build-sys/build-tools.mk
@@ -8,12 +8,26 @@ endif
 
 # TOOLS AND DEFAULT TOOL FLAGS :=========================================================
 
+ifeq ($(OS),Windows_NT)
+SDCC_INSTALLDIR := "/Program Files/SDCC_3_8_0"
+else
+SDCC_INSTALLDIR := /system/usr/share/sdcc
+endif
+
 # SDCC Toolchain Version:
+ifeq ($(OS),Windows_NT)
+SDCC_VER := NOT_IMPLEMENTED
+SDCC_VER_MAJ := NOT_IMPLEMENTED
+SDCC_VER_MIN := NOT_IMPLEMENTED
+SDCC_VER_PAT := NOT_IMPLEMENTED
+SDCC_REV := NOT_IMPLEMENTED
+else
 SDCC_VER := $(shell sdcc --version | grep -P '[0-9]*\.[0-9]*\.[0-9]*' -o | tr . _)
 SDCC_VER_MAJ := $(shell echo $(SDCC_VER) | cut -d '_' -f 1)
 SDCC_VER_MIN := $(shell echo $(SDCC_VER) | cut -d '_' -f 2)
 SDCC_VER_PAT := $(shell echo $(SDCC_VER) | cut -d '_' -f 3)
 SDCC_REV := $(shell sdcc --version | grep -P '\#[0-9]*' -o | tr -d '\#')
+endif
 
 # C Pre-Processor and Flags:
 CPP				:= $(TOOL_PRE)sdcpp
